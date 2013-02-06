370e7270ae73e811729a60bf33b82f6511c3c965
qiushuiyibing@gmail.com <qiushuiyibing@gmail.com@548eb24d-4046-2f48-6424-50ed1c85a2c7>
2013-02-06 14:11:50 +0000

    update xcache
    git-svn-id: https://teddysun.googlecode.com/svn/trunk@35 548eb24d-4046-2f48-6424-50ed1c85a2c7

--- lamp1.2/xcache_2.0.1.sh
+++ lamp1.2/xcache_2.0.1.sh
@@ -28,8 +28,9 @@ tar xzf xcache-2.0.1.tar.gz -C $cur_dir/untar/
 cd $cur_dir/untar/xcache-2.0.1
 export PHP_PREFIX="/usr/local/php"
 $PHP_PREFIX/bin/phpize
-./configure --enable-xcache --enable-xcache-optimizer --enable-xcache-coverager -with-php-config=$PHP_PREFIX/bin/php-config
-make && make install
+./configure --enable-xcache -with-php-config=$PHP_PREFIX/bin/php-config
+make install
+rm -rf /data/www/default/xcache
 cp -r admin/ /data/www/default/xcache
 chmod -R 755 /data/www/default/xcache
 rm -rf /tmp/{pcov,phpcore}
@@ -42,35 +43,36 @@ cat >>/usr/local/php/etc/php.ini<<-EOF
 extension = /usr/local/php/lib/php/extensions/no-debug-non-zts-20090626/xcache.so
 
 [xcache.admin]
-xcache.admin.user = admin
-xcache.admin.pass = e10adc3949ba59abbe56e057f20f883e
+xcache.admin.user = "admin"
+xcache.admin.pass = "e10adc3949ba59abbe56e057f20f883e"
 xcache.admin.enable_auth = On
 xcache.test = Off
-xcache.coredump_directory = /tmp/phpcore
+xcache.coredump_directory = "/tmp/phpcore"
 xcache.disable_on_crash = ""
 
 [xcache]
 xcache.cacher = On
 xcache.size = 64M
-xcache.count = 4
+xcache.count = 1
 xcache.slots = 8K
 xcache.ttl = 3600
 xcache.gc_interval = 60
 xcache.var_size = 1M
-xcache.var_count = 4
+xcache.var_count = 1
 xcache.var_slots = 8K
 xcache.var_ttl = 3600
 xcache.var_maxttl = 0
 xcache.var_gc_interval = 60
 xcache.readonly_protection = Off
-xcache.mmap_path = /dev/zero
+xcache.mmap_path = "/dev/zero"
 
 [xcache.optimizer]
 xcache.optimizer = On
 
 [xcache.coverager]
-xcache.coverager = On
-xcache.coveragedump_directory = /tmp/pcov
+xcache.coverager = Off
+xcache.coverager_autostart =  On
+xcache.coveragedump_directory = "/tmp/pcov"
 EOF
 service httpd restart
 echo "============================Xcache2.0.1 install completed================================="
