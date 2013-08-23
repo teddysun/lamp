97a28f862dd6ece4b569fbee1c0aac045116b4de
i@teddysun.com <i@teddysun.com@548eb24d-4046-2f48-6424-50ed1c85a2c7>
2013-08-23 09:20:10 +0000

    git-svn-id: https://teddysun.googlecode.com/svn/trunk@94 548eb24d-4046-2f48-6424-50ed1c85a2c7

---
+++ lamp2.2/Changelog.md
@@ -0,0 +1,111 @@
+###版本`2.2`：`Release date:2013/08/23`
+更新记录：
+
+* 1、升级`phpMyAdmin`到版本为`4.0.5`；
+* 2、升级`PHP`到版本为`5.4.19`；
+* 3、升级`MySQL`到版本`5.6.13`；
+* 4、优化升级脚本`update.sh`。`PHP5.5.x`系列`Release`没多久，待测试其兼容性后再做升级，因此修改为升级`PHP`的版本为`5.4.x`系列的最新版。
+
+###版本`2.1`：`Release date:2013/07/24`
+更新记录：
+
+* 1、升级`MySQL`到版本`5.6.12`；
+* 2、升级`PHP`到版本为`5.4.17`；
+* 3、升级`Apache`到版本`2.4.6`；
+* 4、升级`phpMyAdmin`到版本`4.0.4.1`；
+* 5、升级`Apache`的依赖包`apr`到版本`1.4.8`；
+* 6、升级`Xcache`到版本`3.0.3`，其安装脚本名从`xcache_3.0.1.sh`改为`xcache_3.0.3.sh`。
+
+
+###版本`2.0`：`Release date:2013/05/13`
+更新记录：
+
+* 1、升级`phpMyAdmin`到版本为`4.0.0`；
+* 2、升级`PHP`到版本为`5.4.15`；
+* 3、优化脚本`update.sh`，修复了安装完新版`PHP`后已安装的`extensions`丢失的`bug`。
+
+**备注：**版本号不作调整，依旧为2.0，替换了原来的下载文件。
+
+###版本`2.0`：`Release date:2013/04/28`
+更新记录：
+
+* 1、升级`MySQL`版本为`5.6.11`；
+* 2、升级`phpMyAdmin`到版本为`3.5.8.1`；
+* 3、增加升级`PHP`和`phpMyAdmin`脚本`update.sh`,可以自动检测最新版`PHP`和`phpMyAdmin`供选择升级;
+* 4、合并原来禁止`SELINUX`的脚本`disable.sh`到`lamp.sh`中。
+
+###版本`1.3.1`：`Release date:2013/04/13`
+更新记录：
+
+* 1、升级`PHP`到版本`5.4.14`。
+
+###版本`1.3`：`Release date:2013/04/11`
+更新记录：
+
+* 1、升级`PHP`到版本`5.4.13`；
+* 2、升级`MySQL`版本为`5.6.10`；
+* 3、升级`phpMyAdmin`到版本为`3.5.8`；
+* 4、升级`Apache`到版本为`2.4.4`；
+* 5、升级`apr-util`到版本`1.5.2`；
+* 6、优化`oci8_oracle`脚本，配置其`extension为no-debug-non-zts-20100525`；
+* 7、优化`xcache`脚本，配置其`extension为no-debug-non-zts-20100525`；
+* 8、升级`PHP`探针文件到最新版`v0.4.7`；
+* 9、因`ZendGuardLoader`只适用于`PHP5.3.x`系列，故在此版本中去除。
+
+###版本`1.2.1`：`Release date:2013/02/22`
+更新记录：
+
+* 1、升级`PHP`版本为`5.3.22`；
+* 2、升级`phpMyAdmin`版本为`3.5.7`；
+* 3、优化`lamp.sh`，增加创建（删除，列出）虚拟主机、创建（删除，列出）ftp用户命令；
+* 4、优化`php.ini`配置文件。
+
+
+###版本`1.2`：`Release date:2013/02/06`
+更新记录：
+
+* 1、升级`MySQL`版本为`5.5.30`；
+* 2、升级`PHP`版本为`5.3.21`；
+* 3、更新探针`p.php`文件;
+* 4、升级`Xcache`版本为`2.0.1`(登录`xcache`管理界面：`http://IP地址或域名/xcache`，用户名：`admin`密码：`123456`)；
+* 5、增加`Xcache`版本为`3.0.1`的脚本(强烈推荐安装此版本，加速效果显著，登录管理界面地址同上)；
+* 6、优化安装脚本判断逻辑。
+
+
+###版本`1.1`：`Release date:2013/01/30`
+更新记录：
+
+* 1、去除`Apache`和`PHP`低版本的安装脚本选择；
+* 2、去除`ZendOptimizer 3.3.9`(脚本`zend.sh`) ，`pure-ftpd-1.0.36`。(脚本`pureftpd.sh`)；
+* 3、优化`lamp.sh`脚本，以及相关配置文件；
+* 4、修改`Apache`的`httpd-vhosts.conf`配置文件，改为单一安装。
+* 5、`phpMyAdmin`升级到`3.5.6`，优化配置文件中的默认语言为简体中文。
+
+
+###版本`1.0`：`Release date:2013/01/14`
+
+###适用环境：
+
+* 系统支持：`CentOS-5 (32bit/64bit)或CentOS-6 (32bit/64bit)`
+* 内存要求：`≥256M`
+
+###将会安装:
+
+* 1、`Apache 2.2.22`或`Apache 2.4.3`
+* 2、`MySQL 5.5.29`
+* 3、`PHP 5.2.17`或`PHP 5.3.20 + ZendGuardLoader`
+* 4、`phpMyAdmin 3.5.5`
+* 5、`ZendOptimizer 3.3.9` (可选，只适合`PHP 5.2.17`)
+* 6、`xcache 1.3.2` (可选)
+* 7、`pure-ftpd-1.0.36`（可选）
+
+###安装其它：
+
+* 1、（可选）执行脚本`pureftpd.sh`安装`pure-ftpd-1.0.36`。(命令：`./pureftpd.sh`)
+* 2、（可选）执行脚本`zend.sh`安装`ZendOptimizer 3.3.9`。(命令：`./zend.sh`) 注意：不适用于`PHP 5.3.20`
+* 3、（建议）执行脚本`xcache.sh`安装`xcache 1.3.2`。(命令：`./xcache.sh`)
+* 4、执行脚本`php5.3_oci8_oracle11g.sh`安装`OCI8`扩展以及`oracle-instantclient11.2`（命令：`./php5.3_oci8_oracle11g.sh`）
+* 5、执行脚本`php5.3_oci8_oracle10g.sh`安装`OCI8`扩展以及`oracle-instantclient10.2`（命令：`./php5.3_oci8_oracle10g.sh`）
+
+**备注：**4、5两者选其一执行即可（可选）。该脚本是为了使PHP可以连接Oracle数据库。若连接的数据库版本为10.2，则执行5，否则执行4。
+
---
+++ lamp2.2/update.sh
@@ -0,0 +1,233 @@
+#!/bin/bash
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+export PATH
+#===============================================================================================
+#   System Required:  CentOS5.x (32bit/64bit) or CentOS6.x (32bit/64bit)
+#   Description:  Auto Update Script for PHP && phpMyAdmin
+#   Author: Teddysun <i@teddysun.com>
+#   Intro:  http://teddysun.com/lamp
+#===============================================================================================
+if [[ $EUID -ne 0 ]]; then
+   echo "Error:This script must be run as root!" 1>&2
+   exit 1
+fi
+cur_dir=`pwd`
+cd $cur_dir
+yum install -y elinks
+
+clear
+echo "#############################################################"
+echo "# Auto Update Script for PHP && phpMyAdmin"
+echo "# System Required:  CentOS5.x (32bit/64bit) or CentOS6.x (32bit/64bit)"
+echo "# Intro: http://teddysun.com/lamp"
+echo "#"
+echo "# Author: Teddysun <i@teddysun.com>"
+echo "#"
+echo "#############################################################"
+echo ""
+
+#Description:PHP5 Update
+if [ ! -s /usr/bin/php ]; then
+	ln -s /usr/local/php/bin/php /usr/bin/php
+fi
+
+LATEST_PHP=$(curl -s http://www.php.net/downloads.php | awk '/Old stable/{print $3}' | grep '5.4')
+INSTALLED_PHP=$(php -r 'echo PHP_VERSION;' 2>/dev/null);
+
+echo -e "Latest version of PHP: \033[41;37m $LATEST_PHP \033[0m"
+echo -e "Installed version of PHP: \033[41;37m $INSTALLED_PHP \033[0m"
+echo ""
+echo "Do you want to upgrade PHP5 ? (y/n)"
+read -p "(Default: n):" UPGRADE_PHP
+if [ -z $UPGRADE_PHP ]; then
+	UPGRADE_PHP="n"
+fi
+echo "---------------------------"
+echo "You choose = $UPGRADE_PHP"
+echo "---------------------------"
+echo ""
+
+#Description:phpMyAdmin Update
+if [ -d /data/www/default/phpmyadmin ]; then
+	INSTALLED_PMA=$(awk '/Version/{print $2}' /data/www/default/phpmyadmin/README)
+else
+	if [ -s "$cur_dir/pmaversion.txt" ]; then
+		INSTALLED_PMA=$(awk '/phpmyadmin/{print $2}' $cur_dir/pmaversion.txt)
+	else
+		echo -e "phpmyadmin\t0" > $cur_dir/pmaversion.txt
+		INSTALLED_PMA=$(awk '/phpmyadmin/{print $2}' $cur_dir/pmaversion.txt)
+	fi
+fi
+
+LATEST_PMA=$(elinks http://nchc.dl.sourceforge.net/project/phpmyadmin/phpMyAdmin/ | awk -F/ '{print $7F}' | sort -n | grep -iv '-' | tail -1)
+echo -e "Latest version of phpmyadmin: \033[41;37m $LATEST_PMA \033[0m"
+echo -e "Installed version of phpmyadmin: \033[41;37m $INSTALLED_PMA \033[0m"
+echo ""
+echo "Do you want to upgrade phpmyadmin ? (y/n)"
+read -p "(Default: n):" UPGRADE_PMA
+if [ -z $UPGRADE_PMA ]; then
+	UPGRADE_PMA="n"
+fi
+echo "---------------------------"
+echo "You choose = $UPGRADE_PMA"
+echo "---------------------------"
+echo ""
+get_char()
+{
+SAVEDSTTY=`stty -g`
+stty -echo
+stty cbreak
+dd if=/dev/tty bs=1 count=1 2> /dev/null
+stty -raw
+stty echo
+stty $SAVEDSTTY
+}
+echo ""
+echo "Press any key to start...or Press Ctrl+C to cancel"
+
+#Description:Download && Untar files
+function untar(){
+	local TARBALL_TYPE
+	if [ -n $1 ]; then
+		SOFTWARE_NAME=`echo $1 | awk -F/ '{print $NF}'`
+		TARBALL_TYPE=`echo $1 | awk -F. '{print $NF}'`
+		wget -c -t3 -T3 $1 -P $cur_dir/
+		if [ $? != "0" ];then
+			rm -rf $cur_dir/$SOFTWARE_NAME
+			wget -c -t3 -T60 $2 -P $cur_dir/
+			SOFTWARE_NAME=`echo $2 | awk -F/ '{print $NF}'`
+			TARBALL_TYPE=`echo $2 | awk -F. '{print $NF}'`
+		fi
+	else
+		SOFTWARE_NAME=`echo $2 | awk -F/ '{print $NF}'`
+		TARBALL_TYPE=`echo $2 | awk -F. '{print $NF}'`
+		wget -c -t3 -T3 $2 -P $cur_dir/ || exit
+	fi
+	EXTRACTED_DIR=`tar tf $cur_dir/$SOFTWARE_NAME | tail -n 1 | awk -F/ '{print $1}'`
+	case $TARBALL_TYPE in
+		gz|tgz)
+			tar zxf $cur_dir/$SOFTWARE_NAME -C $cur_dir/ && cd $cur_dir/$EXTRACTED_DIR || return 1
+		;;
+		bz2|tbz)
+			tar jxf $cur_dir/$SOFTWARE_NAME -C $cur_dir/ && cd $cur_dir/$EXTRACTED_DIR || return 1
+		;;
+		tar|Z)
+			tar xf $cur_dir/$SOFTWARE_NAME -C $cur_dir/ && cd $cur_dir/$EXTRACTED_DIR || return 1
+		;;
+		*)
+		echo "$SOFTWARE_NAME is wrong tarball type ! "
+	esac
+}
+
+#Description:PHP5 Update
+if [[ "$UPGRADE_PHP" = "y" || "$UPGRADE_PHP" = "Y" ]];then
+	echo "===================== PHP5.4.x Upgrade ===================="
+	if [[ -d "/usr/local/php.bak" && -d "/usr/local/php" ]];then
+		rm -rf /usr/local/php.bak/
+	fi
+	\mv /usr/local/php /usr/local/php.bak
+	cd $cur_dir
+	if [ ! -s php-${LATEST_PHP}.tar.gz ]; then
+		LATEST_PHP_LINK="http://us.php.net/distributions/php-${LATEST_PHP}.tar.gz"
+		BACKUP_PHP_LINK="http://teddysun.googlecode.com/files/php-${LATEST_PHP}.tar.gz"
+		untar ${LATEST_PHP_LINK} ${BACKUP_PHP_LINK}
+	else
+		tar -zxf php-${LATEST_PHP}.tar.gz
+		cd php-${LATEST_PHP}/
+	fi
+	
+	./configure \
+	--prefix=/usr/local/php \
+	--with-apxs2=/usr/local/apache/bin/apxs  \
+	--with-config-file-path=/usr/local/php/etc \
+	--with-mysqli=/usr/local/mysql/bin/mysql_config \
+	--with-mysql-sock=/usr/local/mysql/mysql.sock \
+	--with-config-file-scan-dir=/usr/local/php/php.d \
+	--with-openssl \
+	--with-zlib \
+	--with-curl \
+	--enable-ftp \
+	--with-gd \
+	--with-jpeg-dir \
+	--with-png-dir \
+	--with-freetype-dir \
+	--with-xmlrpc \
+	--enable-calendar \
+	--with-imap \
+	--with-kerberos \
+	--with-imap-ssl \
+	--with-ldap \
+	--enable-bcmath \
+	--enable-exif \
+	--enable-wddx \
+	--enable-tokenizer \
+	--enable-simplexml \
+	--enable-sockets \
+	--enable-ctype \
+	--enable-gd-native-ttf \
+	--enable-mbstring \
+	--enable-intl \
+	--enable-xml \
+	--enable-dom \
+	--enable-json \
+	--enable-session \
+	--enable-soap \
+	--with-mcrypt \
+	--enable-zip \
+	--with-iconv=/usr/local/libiconv \
+	--with-mysql=/usr/local/mysql \
+	--with-icu-dir=/usr \
+	--with-mhash=/usr \
+	--with-pcre-dir \
+	--without-pear
+	
+	make && make install
+	mkdir -p /usr/local/php/etc
+	mkdir -p /usr/local/php/php.d
+	mkdir -p /usr/local/php/lib/php/extensions/no-debug-non-zts-20100525
+	\cp -f /usr/local/php.bak/etc/php.ini /usr/local/php/etc/php.ini
+	\cp -f /usr/local/php.bak/php.d/* /usr/local/php/php.d/*
+	\cp -f /usr/local/php.bak/lib/php/extensions/no-debug-non-zts-20100525/* /usr/local/php/lib/php/extensions/no-debug-non-zts-20100525/
+	#Restart httpd service
+	service httpd restart
+	echo "===================== PHP5.4.x Update completed! ===================="
+	echo ""
+fi
+
+#Description:phpMyAdmin Update
+if [[ "$UPGRADE_PMA" = "y" || "$UPGRADE_PMA" = "Y" ]];then
+	echo "===================== phpMyAdmin Upgrade ===================="
+	if [ -d /data/www/default/phpmyadmin ]; then
+		mv /data/www/default/phpmyadmin/config.inc.php $cur_dir/config.inc.php
+		rm -rf /data/www/default/phpmyadmin
+	else
+		echo "===================== phpMyAdmin folder not found! ===================="
+	fi
+	if [ ! -s phpMyAdmin-${LATEST_PMA}-all-languages.tar.gz ]; then
+		LATEST_PMA_LINK="http://nchc.dl.sourceforge.net/project/phpmyadmin/phpMyAdmin/${LATEST_PMA}/phpMyAdmin-${LATEST_PMA}-all-languages.tar.gz"
+		BACKUP_PMA_LINK="http://teddysun.googlecode.com/files/phpMyAdmin-${LATEST_PMA}-all-languages.tar.gz"
+		untar ${LATEST_PMA_LINK} ${BACKUP_PMA_LINK}
+		mkdir -p /data/www/default/phpmyadmin
+		mv * /data/www/default/phpmyadmin
+	else
+		tar -zxf phpMyAdmin-${LATEST_PMA}-all-languages.tar.gz -C $cur_dir
+		mv $cur_dir/phpMyAdmin-${LATEST_PMA}-all-languages /data/www/default/phpmyadmin
+	fi
+	if [ -s $cur_dir/config.inc.php ]; then
+		mv $cur_dir/config.inc.php /data/www/default/phpmyadmin/config.inc.php
+	else
+		mv /data/www/default/phpmyadmin/config.sample.inc.php /data/www/default/phpmyadmin/config.inc.php
+	fi
+	mkdir -p /data/www/default/phpmyadmin/upload/
+	mkdir -p /data/www/default/phpmyadmin/save/
+	chown -R apache:apache /data/www/default/phpmyadmin
+	
+	cd $cur_dir
+	rm -rf $cur_dir/pmaversion.txt
+	echo -e "phpmyadmin\t${LATEST_PMA}" > $cur_dir/pmaversion.txt
+	rm -rf $cur_dir/phpMyAdmin-${LATEST_PMA}-all-languages
+	#Reload httpd service
+	service httpd reload
+	echo "===================== phpMyAdmin Update completed! ===================="
+	echo ""
+fi
