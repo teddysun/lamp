5e81fc1e2d08b39facb428c81f225989014dc173
Teddysun <i@teddysun.com>
2013-12-10 12:01:04 +0800

    Update oracle-instantclient to latest version

--- lamp2.2/php5.4_oci8_oracle11g.sh
+++ lamp2.2/php5.4_oci8_oracle11g.sh
@@ -16,8 +16,8 @@ export PATH
 function rootness(){
 # Make sure only root can run our script
 if [[ $EUID -ne 0 ]]; then
-   echo "This script must be run as root" 1>&2
-   exit 1
+    echo "This script must be run as root" 1>&2
+    exit 1
 fi
 #
 cur_dir=`pwd`
@@ -32,13 +32,13 @@ yum -y install libaio
 #===============================================================================
 function download_files(){
 if [ -s $1 ]; then
-  echo "$1 [found]"
+    echo "$1 [found]"
 else
-  echo "$1 not found!!!download now......"
- if ! wget --tries=3 http://teddysun.googlecode.com/files/$1;then
- echo "Failed to download $1,please download it to "$cur_dir" directory manually and rerun the install script."
- exit 1
- fi
+   echo "$1 not found!!!download now......"
+   if ! wget --tries=3 http://teddysun.googlecode.com/files/$1;then
+       echo "Failed to download $1,please download it to "$cur_dir" directory manually and rerun the install script."
+       exit 1
+   fi
 fi
 }
 
@@ -48,11 +48,11 @@ fi
 #===============================================================================
 function install_instant(){
 if [ `getconf WORD_BIT` = '32' ] && [ `getconf LONG_BIT` = '64' ] ; then
-	rpm -ivh oracle-instantclient11.2-basic-11.2.0.3.0-1.x86_64.rpm
-	rpm -ivh oracle-instantclient11.2-devel-11.2.0.3.0-1.x86_64.rpm
+    rpm -ivh oracle-instantclient11.2-basic-11.2.0.4.0-1.x86_64.rpm
+    rpm -ivh oracle-instantclient11.2-devel-11.2.0.4.0-1.x86_64.rpm
 else
-	rpm -ivh oracle-instantclient11.2-basic-11.2.0.3.0-1.i386.rpm
-	rpm -ivh oracle-instantclient11.2-devel-11.2.0.3.0-1.i386.rpm
+    rpm -ivh oracle-instantclient11.2-basic-11.2.0.4.0-1.i386.rpm
+    rpm -ivh oracle-instantclient11.2-devel-11.2.0.4.0-1.i386.rpm
 fi
 }
 
@@ -61,13 +61,13 @@ fi
 #USAGE:recompile_oci8
 #===============================================================================
 function recompile_oci8(){
-echo "============================oci8 install======================================================="
-cd $cur_dir/untar/oci8-2.0.6
-export PHP_PREFIX="/usr/local/php"
-$PHP_PREFIX/bin/phpize
-./configure --with-php-config=$PHP_PREFIX/bin/php-config
-make && make install
-cat >>/usr/local/php/etc/php.ini<<-EOF
+    echo "============================oci8 install======================================================="
+    cd $cur_dir/untar/oci8-2.0.6
+    export PHP_PREFIX="/usr/local/php"
+    $PHP_PREFIX/bin/phpize
+    ./configure --with-php-config=$PHP_PREFIX/bin/php-config
+    make && make install
+    cat >>/usr/local/php/etc/php.ini<<-EOF
 
 [OCI8]
 extension = /usr/local/php/lib/php/extensions/no-debug-non-zts-20100525/oci8.so
@@ -83,8 +83,8 @@ oci8.default_prefetch = 100
 oci8.old_oci_close_semantics = Off
 EOF
 #
-service httpd restart
-echo "============================oci8 install completed============================================"
+    service httpd restart
+    echo "============================oci8 install completed============================================"
 exit
 }
 #===============================================================================
@@ -92,20 +92,20 @@ exit
 #USAGE:install_oci8
 #===============================================================================
 function install_oci8(){
-rootness
-download_files "oci8-2.0.6.tgz"
-if [ `getconf WORD_BIT` = '32' ] && [ `getconf LONG_BIT` = '64' ] ; then
-	download_files "oracle-instantclient11.2-basic-11.2.0.3.0-1.x86_64.rpm"
-	download_files "oracle-instantclient11.2-devel-11.2.0.3.0-1.x86_64.rpm"
-else
-	download_files "oracle-instantclient11.2-basic-11.2.0.3.0-1.i386.rpm"
-	download_files "oracle-instantclient11.2-devel-11.2.0.3.0-1.i386.rpm"
-fi
-rm -rf $cur_dir/untar
-mkdir -p $cur_dir/untar
-tar xzf oci8-2.0.6.tgz -C $cur_dir/untar/
-install_instant
-recompile_oci8
+    rootness
+    download_files "oci8-2.0.6.tgz"
+    if [ `getconf WORD_BIT` = '32' ] && [ `getconf LONG_BIT` = '64' ] ; then
+        download_files "oracle-instantclient11.2-basic-11.2.0.4.0-1.x86_64.rpm"
+        download_files "oracle-instantclient11.2-devel-11.2.0.4.0-1.x86_64.rpm"
+    else
+        download_files "oracle-instantclient11.2-basic-11.2.0.4.0-1.i386.rpm"
+        download_files "oracle-instantclient11.2-devel-11.2.0.4.0-1.i386.rpm"
+    fi
+    rm -rf $cur_dir/untar
+    mkdir -p $cur_dir/untar
+    tar xzf oci8-2.0.6.tgz -C $cur_dir/untar/
+    install_instant
+    recompile_oci8
 }
 
 action=$1
