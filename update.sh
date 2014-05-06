df7d28e54e33fbc5ac5c515fcac6245f82016ccb
Teddysun <i@teddysun.com>
2014-05-06 11:56:03 +0800

    Fix some little problems

--- update.sh
+++ update.sh
@@ -13,7 +13,6 @@ if [[ $EUID -ne 0 ]]; then
 fi
 cur_dir=`pwd`
 cd $cur_dir
-yum install -y elinks
 
 clear
 echo "#############################################################"
@@ -28,7 +27,7 @@ echo ""
 
 #Description:PHP5 Update
 if [ ! -s /usr/bin/php ]; then
-	ln -s /usr/local/php/bin/php /usr/bin/php
+    ln -s /usr/local/php/bin/php /usr/bin/php
 fi
 
 LATEST_PHP=$(curl -s http://www.php.net/downloads.php | awk '/Changelog/{print $2}' | grep '5.4')
@@ -40,7 +39,7 @@ echo ""
 echo "Do you want to upgrade PHP5 ? (y/n)"
 read -p "(Default: n):" UPGRADE_PHP
 if [ -z $UPGRADE_PHP ]; then
-	UPGRADE_PHP="n"
+    UPGRADE_PHP="n"
 fi
 echo "---------------------------"
 echo "You choose = $UPGRADE_PHP"
@@ -49,14 +48,14 @@ echo ""
 
 #Description:phpMyAdmin Update
 if [ -d /data/www/default/phpmyadmin ]; then
-	INSTALLED_PMA=$(awk '/Version/{print $2}' /data/www/default/phpmyadmin/README)
+    INSTALLED_PMA=$(awk '/Version/{print $2}' /data/www/default/phpmyadmin/README)
 else
-	if [ -s "$cur_dir/pmaversion.txt" ]; then
-		INSTALLED_PMA=$(awk '/phpmyadmin/{print $2}' $cur_dir/pmaversion.txt)
-	else
-		echo -e "phpmyadmin\t0" > $cur_dir/pmaversion.txt
-		INSTALLED_PMA=$(awk '/phpmyadmin/{print $2}' $cur_dir/pmaversion.txt)
-	fi
+    if [ -s "$cur_dir/pmaversion.txt" ]; then
+        INSTALLED_PMA=$(awk '/phpmyadmin/{print $2}' $cur_dir/pmaversion.txt)
+    else
+        echo -e "phpmyadmin\t0" > $cur_dir/pmaversion.txt
+        INSTALLED_PMA=$(awk '/phpmyadmin/{print $2}' $cur_dir/pmaversion.txt)
+    fi
 fi
 
 LATEST_PMA=$(elinks http://iweb.dl.sourceforge.net/project/phpmyadmin/phpMyAdmin/ | awk -F/ '{print $7F}' | grep -iv '-' | grep -iv 'rst' | tail -1)
@@ -66,7 +65,7 @@ echo ""
 echo "Do you want to upgrade phpmyadmin ? (y/n)"
 read -p "(Default: n):" UPGRADE_PMA
 if [ -z $UPGRADE_PMA ]; then
-	UPGRADE_PMA="n"
+    UPGRADE_PMA="n"
 fi
 echo "---------------------------"
 echo "You choose = $UPGRADE_PMA"
@@ -87,149 +86,150 @@ echo "Press any key to start...or Press Ctrl+C to cancel"
 
 #Description:Download && Untar files
 function untar(){
-	local TARBALL_TYPE
-	if [ -n $1 ]; then
-		SOFTWARE_NAME=`echo $1 | awk -F/ '{print $NF}'`
-		TARBALL_TYPE=`echo $1 | awk -F. '{print $NF}'`
-		wget -c -t3 -T3 $1 -P $cur_dir/
-		if [ $? != "0" ];then
-			rm -rf $cur_dir/$SOFTWARE_NAME
-			wget -c -t3 -T60 $2 -P $cur_dir/
-			SOFTWARE_NAME=`echo $2 | awk -F/ '{print $NF}'`
-			TARBALL_TYPE=`echo $2 | awk -F. '{print $NF}'`
-		fi
-	else
-		SOFTWARE_NAME=`echo $2 | awk -F/ '{print $NF}'`
-		TARBALL_TYPE=`echo $2 | awk -F. '{print $NF}'`
-		wget -c -t3 -T3 $2 -P $cur_dir/ || exit
-	fi
-	EXTRACTED_DIR=`tar tf $cur_dir/$SOFTWARE_NAME | tail -n 1 | awk -F/ '{print $1}'`
-	case $TARBALL_TYPE in
-		gz|tgz)
-			tar zxf $cur_dir/$SOFTWARE_NAME -C $cur_dir/ && cd $cur_dir/$EXTRACTED_DIR || return 1
-		;;
-		bz2|tbz)
-			tar jxf $cur_dir/$SOFTWARE_NAME -C $cur_dir/ && cd $cur_dir/$EXTRACTED_DIR || return 1
-		;;
-		tar|Z)
-			tar xf $cur_dir/$SOFTWARE_NAME -C $cur_dir/ && cd $cur_dir/$EXTRACTED_DIR || return 1
-		;;
-		*)
-		echo "$SOFTWARE_NAME is wrong tarball type ! "
-	esac
+    local TARBALL_TYPE
+    if [ -n $1 ]; then
+        SOFTWARE_NAME=`echo $1 | awk -F/ '{print $NF}'`
+        TARBALL_TYPE=`echo $1 | awk -F. '{print $NF}'`
+        wget -c -t3 -T3 $1 -P $cur_dir/
+        if [ $? != "0" ];then
+            rm -rf $cur_dir/$SOFTWARE_NAME
+            wget -c -t3 -T60 $2 -P $cur_dir/
+            SOFTWARE_NAME=`echo $2 | awk -F/ '{print $NF}'`
+            TARBALL_TYPE=`echo $2 | awk -F. '{print $NF}'`
+        fi
+    else
+        SOFTWARE_NAME=`echo $2 | awk -F/ '{print $NF}'`
+        TARBALL_TYPE=`echo $2 | awk -F. '{print $NF}'`
+        wget -c -t3 -T3 $2 -P $cur_dir/ || exit
+    fi
+    EXTRACTED_DIR=`tar tf $cur_dir/$SOFTWARE_NAME | tail -n 1 | awk -F/ '{print $1}'`
+    case $TARBALL_TYPE in
+        gz|tgz)
+            tar zxf $cur_dir/$SOFTWARE_NAME -C $cur_dir/ && cd $cur_dir/$EXTRACTED_DIR || return 1
+        ;;
+        bz2|tbz)
+            tar jxf $cur_dir/$SOFTWARE_NAME -C $cur_dir/ && cd $cur_dir/$EXTRACTED_DIR || return 1
+        ;;
+        tar|Z)
+            tar xf $cur_dir/$SOFTWARE_NAME -C $cur_dir/ && cd $cur_dir/$EXTRACTED_DIR || return 1
+        ;;
+        *)
+        echo "$SOFTWARE_NAME is wrong tarball type ! "
+    esac
 }
 
 #Description:PHP5 Update
 if [[ "$UPGRADE_PHP" = "y" || "$UPGRADE_PHP" = "Y" ]];then
-	echo "===================== PHP5.4.x Upgrade ===================="
-	if [[ -d "/usr/local/php.bak" && -d "/usr/local/php" ]];then
-		rm -rf /usr/local/php.bak/
-	fi
-	\mv /usr/local/php /usr/local/php.bak
-	cd $cur_dir
-	if [ ! -s php-${LATEST_PHP}.tar.gz ]; then
-		LATEST_PHP_LINK="http://us1.php.net/distributions/php-${LATEST_PHP}.tar.gz"
-		BACKUP_PHP_LINK="http://lamp.teddysun.com/files/php-${LATEST_PHP}.tar.gz"
-		untar ${LATEST_PHP_LINK} ${BACKUP_PHP_LINK}
-	else
-		tar -zxf php-${LATEST_PHP}.tar.gz
-		cd php-${LATEST_PHP}/
-	fi
-	
-	./configure \
-	--prefix=/usr/local/php \
-	--with-apxs2=/usr/local/apache/bin/apxs  \
-	--with-config-file-path=/usr/local/php/etc \
-	--with-mysqli=/usr/local/mysql/bin/mysql_config \
-	--with-pdo-mysql \
-	--with-mysql-sock=/usr/local/mysql/mysql.sock \
-	--with-config-file-scan-dir=/usr/local/php/php.d \
-	--with-openssl \
-	--with-zlib \
-	--with-curl \
-	--enable-ftp \
-	--with-gd \
-	--with-jpeg-dir \
-	--with-png-dir \
-	--with-freetype-dir \
-	--with-xmlrpc \
-	--enable-calendar \
-	--with-imap \
-	--with-kerberos \
-	--with-imap-ssl \
-	--with-ldap \
-	--enable-bcmath \
-	--enable-exif \
-	--enable-wddx \
-	--enable-tokenizer \
-	--enable-simplexml \
-	--enable-sockets \
-	--enable-ctype \
-	--enable-gd-native-ttf \
-	--enable-mbstring \
-	--enable-intl \
-	--enable-xml \
-	--enable-dom \
-	--enable-json \
-	--enable-session \
-	--enable-soap \
-	--with-mcrypt \
-	--enable-zip \
-	--with-iconv=/usr/local/libiconv \
-	--with-mysql=/usr/local/mysql \
-	--with-icu-dir=/usr \
-	--with-mhash=/usr \
-	--with-pcre-dir \
-	--without-pear
-	
-	make && make install
-	mkdir -p /usr/local/php/etc
-	mkdir -p /usr/local/php/php.d
-	mkdir -p /usr/local/php/lib/php/extensions/no-debug-non-zts-20100525
-	\cp -f /usr/local/php.bak/etc/php.ini /usr/local/php/etc/php.ini
-	\cp -f /usr/local/php.bak/php.d/* /usr/local/php/php.d/*
-	\cp -f /usr/local/php.bak/lib/php/extensions/no-debug-non-zts-20100525/* /usr/local/php/lib/php/extensions/no-debug-non-zts-20100525/
-	#Restart httpd service
-	service httpd restart
-	echo "===================== PHP5.4.x Update completed! ===================="
-	echo ""
+    echo "===================== PHP5.4.x Upgrade ===================="
+    if [[ -d "/usr/local/php.bak" && -d "/usr/local/php" ]];then
+        rm -rf /usr/local/php.bak/
+    fi
+    mv /usr/local/php /usr/local/php.bak
+    cd $cur_dir
+    if [ ! -s php-${LATEST_PHP}.tar.gz ]; then
+        LATEST_PHP_LINK="http://php.net/distributions/php-${LATEST_PHP}.tar.gz"
+        BACKUP_PHP_LINK="http://lamp.teddysun.com/files/php-${LATEST_PHP}.tar.gz"
+        untar ${LATEST_PHP_LINK} ${BACKUP_PHP_LINK}
+    else
+        tar -zxf php-${LATEST_PHP}.tar.gz
+        cd php-${LATEST_PHP}/
+    fi
+    
+    ./configure \
+    --prefix=/usr/local/php \
+    --with-apxs2=/usr/local/apache/bin/apxs  \
+    --with-config-file-path=/usr/local/php/etc \
+    --with-mysqli=/usr/local/mysql/bin/mysql_config \
+    --with-pdo-mysql \
+    --with-mysql-sock=/usr/local/mysql/mysql.sock \
+    --with-config-file-scan-dir=/usr/local/php/php.d \
+    --with-openssl \
+    --with-zlib \
+    --with-curl \
+    --enable-ftp \
+    --with-gd \
+    --with-jpeg-dir \
+    --with-png-dir \
+    --with-freetype-dir \
+    --with-xmlrpc \
+    --enable-calendar \
+    --with-imap \
+    --with-kerberos \
+    --with-imap-ssl \
+    --with-ldap \
+    --enable-bcmath \
+    --enable-exif \
+    --enable-wddx \
+    --enable-tokenizer \
+    --enable-simplexml \
+    --enable-sockets \
+    --enable-ctype \
+    --enable-gd-native-ttf \
+    --enable-mbstring \
+    --enable-intl \
+    --enable-xml \
+    --enable-dom \
+    --enable-json \
+    --enable-session \
+    --enable-soap \
+    --with-mcrypt \
+    --enable-zip \
+    --with-iconv=/usr/local/libiconv \
+    --with-mysql=/usr/local/mysql \
+    --with-icu-dir=/usr \
+    --with-mhash=/usr \
+    --with-pcre-dir \
+    --without-pear
+    
+    make && make install
+    mkdir -p /usr/local/php/etc
+    mkdir -p /usr/local/php/php.d
+    cp -f /usr/local/php.bak/etc/php.ini /usr/local/php/etc/php.ini
+    cp -f /usr/local/php.bak/lib/php/extensions/no-debug-non-zts-20100525/* /usr/local/php/lib/php/extensions/no-debug-non-zts-20100525/
+    php_d=`ls /usr/local/php.bak/php.d/ | wc -l`
+    if [ $php_d -ne 0 ]; then
+        cp -f /usr/local/php.bak/php.d/* /usr/local/php/php.d/*
+    fi
+    #Restart httpd service
+    service httpd restart
+    echo "===================== PHP5.4.x Update completed! ===================="
+    echo ""
 fi
 
 #Description:phpMyAdmin Update
 if [[ "$UPGRADE_PMA" = "y" || "$UPGRADE_PMA" = "Y" ]];then
-	echo "===================== phpMyAdmin Upgrade ===================="
-	if [ -d /data/www/default/phpmyadmin ]; then
-		mv /data/www/default/phpmyadmin/config.inc.php $cur_dir/config.inc.php
-		rm -rf /data/www/default/phpmyadmin
-	else
-		echo "===================== phpMyAdmin folder not found! ===================="
-	fi
-	if [ ! -s phpMyAdmin-${LATEST_PMA}-all-languages.tar.gz ]; then
-		LATEST_PMA_LINK="http://iweb.dl.sourceforge.net/project/phpmyadmin/phpMyAdmin/${LATEST_PMA}/phpMyAdmin-${LATEST_PMA}-all-languages.tar.gz"
-		BACKUP_PMA_LINK="http://lamp.teddysun.com/files/phpMyAdmin-${LATEST_PMA}-all-languages.tar.gz"
-		untar ${LATEST_PMA_LINK} ${BACKUP_PMA_LINK}
-		mkdir -p /data/www/default/phpmyadmin
-		mv * /data/www/default/phpmyadmin
-	else
-		tar -zxf phpMyAdmin-${LATEST_PMA}-all-languages.tar.gz -C $cur_dir
-		mv $cur_dir/phpMyAdmin-${LATEST_PMA}-all-languages /data/www/default/phpmyadmin
-	fi
-	if [ -s $cur_dir/config.inc.php ]; then
-		mv $cur_dir/config.inc.php /data/www/default/phpmyadmin/config.inc.php
-	else
-		mv /data/www/default/phpmyadmin/config.sample.inc.php /data/www/default/phpmyadmin/config.inc.php
-	fi
-	mkdir -p /data/www/default/phpmyadmin/upload/
-	mkdir -p /data/www/default/phpmyadmin/save/
-	cp -f /data/www/default/phpmyadmin/examples/create_tables.sql /data/www/default/phpmyadmin/upload/
-	chown -R apache:apache /data/www/default/phpmyadmin
-	
-	cd $cur_dir
-	rm -rf $cur_dir/pmaversion.txt
-	echo -e "phpmyadmin\t${LATEST_PMA}" > $cur_dir/pmaversion.txt
-	rm -rf $cur_dir/phpMyAdmin-${LATEST_PMA}-all-languages
-	#Reload httpd service
-	service httpd reload
-	echo "===================== phpMyAdmin Update completed! ===================="
-	echo ""
+    echo "===================== phpMyAdmin Upgrade ===================="
+    if [ -d /data/www/default/phpmyadmin ]; then
+        mv /data/www/default/phpmyadmin/config.inc.php $cur_dir/config.inc.php
+        rm -rf /data/www/default/phpmyadmin
+    else
+        echo "===================== phpMyAdmin folder not found! ===================="
+    fi
+    if [ ! -s phpMyAdmin-${LATEST_PMA}-all-languages.tar.gz ]; then
+        LATEST_PMA_LINK="http://iweb.dl.sourceforge.net/project/phpmyadmin/phpMyAdmin/${LATEST_PMA}/phpMyAdmin-${LATEST_PMA}-all-languages.tar.gz"
+        BACKUP_PMA_LINK="http://lamp.teddysun.com/files/phpMyAdmin-${LATEST_PMA}-all-languages.tar.gz"
+        untar ${LATEST_PMA_LINK} ${BACKUP_PMA_LINK}
+        mkdir -p /data/www/default/phpmyadmin
+        mv * /data/www/default/phpmyadmin
+    else
+        tar -zxf phpMyAdmin-${LATEST_PMA}-all-languages.tar.gz -C $cur_dir
+        mv $cur_dir/phpMyAdmin-${LATEST_PMA}-all-languages /data/www/default/phpmyadmin
+    fi
+    if [ -s $cur_dir/config.inc.php ]; then
+        mv $cur_dir/config.inc.php /data/www/default/phpmyadmin/config.inc.php
+    else
+        mv /data/www/default/phpmyadmin/config.sample.inc.php /data/www/default/phpmyadmin/config.inc.php
+    fi
+    mkdir -p /data/www/default/phpmyadmin/upload/
+    mkdir -p /data/www/default/phpmyadmin/save/
+    cp -f /data/www/default/phpmyadmin/examples/create_tables.sql /data/www/default/phpmyadmin/upload/
+    chown -R apache:apache /data/www/default/phpmyadmin
+    # clean phpMyAdmin archive
+    cd $cur_dir
+    rm -rf $cur_dir/pmaversion.txt
+    echo -e "phpmyadmin\t${LATEST_PMA}" > $cur_dir/pmaversion.txt
+    rm -rf $cur_dir/phpMyAdmin-${LATEST_PMA}-all-languages
+    #Reload httpd service
+    service httpd reload
+    echo "===================== phpMyAdmin Update completed! ===================="
 fi
