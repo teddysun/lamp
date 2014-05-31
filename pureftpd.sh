d5a333c3ad49ae720b8eb40f48ba18c6e6cf8c3c
Teddysun <i@teddysun.com>
2014-05-31 23:45:17 +0800

    Optimize pure-ftpd the method of get config file

--- pureftpd.sh
+++ pureftpd.sh
@@ -19,13 +19,13 @@ if [ -s pure-ftpd-1.0.36.tar.gz ]; then
 else
     echo "pure-ftpd-1.0.36.tar.gz not found!!!download now......"
     if ! wget -c http://teddysun.googlecode.com/files/pure-ftpd-1.0.36.tar.gz;then
-        echo "Failed to download pure-ftpd-1.0.36.tar.gz,please download it to /lamp directory manually and rerun the install script."
+        echo "Failed to download pure-ftpd-1.0.36.tar.gz, please download it to $cur_dir directory manually and try again."
         exit 1
     fi
 fi
 
 #install pure-ftpd 
-echo "============================pure-ftpd  install============================================"
+echo "============================pure-ftpd  install start============================"
 mkdir -p $cur_dir/untar/
 tar xzf pure-ftpd-1.0.36.tar.gz -C $cur_dir/untar/
 cd $cur_dir/untar/pure-ftpd-1.0.36
@@ -35,7 +35,10 @@ cp contrib/redhat.init /etc/init.d/pure-ftpd
 chmod 755 /etc/init.d/pure-ftpd
 chkconfig --add pure-ftpd
 chkconfig --level 3 pure-ftpd on
-cp $cur_dir/conf/pure-ftpd.conf /etc
+if ! wget --no-check-certificate https://github.com/teddysun/lamp/raw/master/conf/pure-ftpd.conf -O /etc/pure-ftpd.conf;then
+    echo "Failed to download pure-ftpd.conf, please download it to /etc directory manually and try again."
+    exit 1
+fi
 cp configuration-file/pure-config.pl /usr/local/sbin/pure-config.pl
 chmod 744 /etc/pure-ftpd.conf
 chmod 755 /usr/local/sbin/pure-config.pl
@@ -43,8 +46,8 @@ service pure-ftpd start
 #see if iptables is start
 /sbin/service iptables status 1>/dev/null 2>&1
 if [ $? -eq 0 ]; then
-/sbin/iptables -A INPUT -m state --state NEW -m tcp -p tcp --dport 21 -j ACCEPT
-/etc/rc.d/init.d/iptables save
-/etc/init.d/iptables restart
+    /sbin/iptables -A INPUT -m state --state NEW -m tcp -p tcp --dport 21 -j ACCEPT
+    /etc/rc.d/init.d/iptables save
+    /etc/init.d/iptables restart
 fi
-echo "============================pure-ftpd install completed============================================"
+echo "============================pure-ftpd install completed============================"
