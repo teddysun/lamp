6a6c569ffe5e03d1b9efd502cb370a5062dd8033
Teddysun <i@teddysun.com>
2014-06-05 13:57:19 +0800

    Optimize Zend Guard Loader configuration file to additional directory

--- ZendGuardLoader.sh
+++ ZendGuardLoader.sh
@@ -47,16 +47,13 @@ else
     mv $cur_dir/untar/ZendGuardLoader-70429-PHP-5.4-linux-glibc23-i386/php-5.4.x/ZendGuardLoader.so /usr/local/php/lib/php/extensions/no-debug-non-zts-20100525/
 fi
 
-zg=`cat /usr/local/php/etc/php.ini |grep -q "Zend Guard" && echo "include" || echo "not"`
-if [ "$zg" = "not" ]; then
-echo "Zend Guard configuration not found, create it!"
-cat >>/usr/local/php/etc/php.ini<<-EOF
-
+if [ ! -f /usr/local/php/php.d/zend.ini ]; then
+    echo "Zend Guard Loader configuration not found, create it!"
+    cat > /usr/local/php/php.d/zend.ini<<-EOF
 [Zend Guard]
 extension = /usr/local/php/lib/php/extensions/no-debug-non-zts-20100525/ZendGuardLoader.so
-; Enables loading encoded scripts. The default value is On
+
 zend_loader.enable = 1
-; Optional: following lines can be added your php.ini file for ZendGuardLoader configuration
 zend_loader.disable_licensing = 0
 zend_loader.obfuscation_level_support = 3
 zend_loader.license_path =
