ccba20573b99c9995b8e9e7d8e333c3d4206bb6d
Teddysun <i@teddysun.com>
2015-09-25 16:16:22 +0800

    Restart Apache only once

--- upgrade_php.sh
+++ upgrade_php.sh
@@ -16,6 +16,7 @@ if [ ! -d /usr/local/php ]; then
 fi
 
 cur_dir=`pwd`
+apache_restart_flg='0'
 
 clear
 echo "#############################################################"
@@ -262,8 +263,8 @@ if [[ "$UPGRADE_PHP" = "y" || "$UPGRADE_PHP" = "Y" ]];then
     cd $cur_dir
     rm -rf php-$LATEST_PHP/
     rm -f php-$LATEST_PHP.tar.gz
-    # Restart httpd service
-    /etc/init.d/httpd restart
+    # Apache restart flag
+    apache_restart_flg='1'
     echo "===================== PHP update completed! ===================="
     echo ""
 else
@@ -311,10 +312,16 @@ if [[ "$UPGRADE_PMA" = "y" || "$UPGRADE_PMA" = "Y" ]];then
     echo -e "phpmyadmin\t${LATEST_PMA}" > $cur_dir/pmaversion.txt
     rm -rf $cur_dir/phpMyAdmin-$LATEST_PMA-all-languages
     rm -f phpMyAdmin-$LATEST_PMA-all-languages.tar.gz
-    # Restart httpd service
-    /etc/init.d/httpd restart
+    # Apache restart flag
+    apache_restart_flg='1'
     echo "===================== phpMyAdmin update completed! ===================="
 else
     echo "phpMyAdmin upgrade cancelled, nothing to do..."
     echo ""
+fi
+
+# Restart Apache
+if [ "$apache_restart_flg" = "1" ]; then
+    # Restart httpd service
+    /etc/init.d/httpd restart
 fi
