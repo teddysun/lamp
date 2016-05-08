56f06dddedade1abd86d68d1fe86ac5d8518baf5
Teddysun <i@teddysun.com>
2016-05-09 00:23:56 +0900

    update apache version

--- include/config.sh
+++ include/config.sh
@@ -22,7 +22,7 @@ parallel_compile=1
 #apache2.2
 apache2_2_filename="httpd-2.2.31"
 #apache2.4
-apache2_4_filename="httpd-2.4.18"
+apache2_4_filename="httpd-2.4.20"
 #mysql5.5
 mysql5_5_filename="mysql-5.5.49"
 #mysql5.6
