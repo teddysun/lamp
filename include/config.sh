6b30d3dd8c62d93d10a2712c0cadaf945ab70817
Teddysun <i@teddysun.com>
2016-07-23 00:11:59 +0900

    update MariaDB version

--- include/config.sh
+++ include/config.sh
@@ -37,7 +37,7 @@ mariadb5_5_filename="mariadb-5.5.50"
 #mariadb10.0
 mariadb10_0_filename="mariadb-10.0.26"
 #mariadb10.1
-mariadb10_1_filename="mariadb-10.1.14"
+mariadb10_1_filename="mariadb-10.1.16"
 #php5.3
 php5_3_filename="php-5.3.29"
 #php5.4
