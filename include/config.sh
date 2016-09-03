1a865e973163a74f1ad01454d553654acdd44049
Teddysun <i@teddysun.com>
2016-09-03 23:28:04 +0900

    update software version

--- include/config.sh
+++ include/config.sh
@@ -38,7 +38,7 @@ mariadb5_5_filename="mariadb-5.5.51"
 #mariadb10.0
 mariadb10_0_filename="mariadb-10.0.27"
 #mariadb10.1
-mariadb10_1_filename="mariadb-10.1.16"
+mariadb10_1_filename="mariadb-10.1.17"
 #percona5.5
 percona5_5_filename="percona-server-5.5.51-38.1"
 set_hint ${percona5_5_filename} "percona-server-5.5.51"
