98e74a58b89d8ceb3540a392c0f6008bb7013311
Teddysun <i@teddysun.com>
2017-03-27 21:30:51 +0900

    update comments
    Signed-off-by: Teddysun <i@teddysun.com>

--- include/upgrade_db.sh
+++ include/upgrade_db.sh
@@ -1,3 +1,16 @@
+# Copyright (C) 2014 - 2017, Teddysun <i@teddysun.com>
+# 
+# This file is part of the LAMP script.
+#
+# LAMP is a powerful bash script for the installation of 
+# Apache + PHP + MySQL/MariaDB/Percona and so on.
+# You can install Apache + PHP + MySQL/MariaDB/Percona in an very easy way.
+# Just need to input numbers to choose what you want to install before installation.
+# And all things will be done in a few minutes.
+#
+# Website:  https://lamp.sh
+# Github:   https://github.com/teddysun/lamp
+
 #upgrade database
 upgrade_db(){
 
--- lamp.sh
+++ lamp.sh
@@ -1,12 +1,21 @@
 #!/usr/bin/env bash
+# Copyright (C) 2014 - 2017, Teddysun <i@teddysun.com>
+# 
+# This file is part of the LAMP script.
+#
+# LAMP is a powerful bash script for the installation of 
+# Apache + PHP + MySQL/MariaDB/Percona and so on.
+# You can install Apache + PHP + MySQL/MariaDB/Percona in an very easy way.
+# Just need to input numbers to choose what you want to install before installation.
+# And all things will be done in a few minutes.
+#
+# System Required:  CentOS 5+ / Debian 7+ / Ubuntu 12+
+# Description:  Install LAMP(Linux + Apache + MySQL/MariaDB/Percona + PHP )
+# Website:  https://lamp.sh
+# Github:   https://github.com/teddysun/lamp
+
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
-#===============================================================================#
-#   System Required:  CentOS/RadHat 5+ / Debian 7+ / Ubuntu 12+                 #
-#   Description:  Install LAMP(Linux + Apache + MySQL/MariaDB/Percona + PHP )   #
-#   Author: Teddysun <i@teddysun.com>                                           #
-#   Intro:  https://lamp.sh                                                     #
-#===============================================================================#
 
 cur_dir=`pwd`
 
