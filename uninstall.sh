73b13c89c01557d0e81006f1fb87490d7af600cb
Teddysun <i@teddysun.com>
2017-04-23 16:23:16 +0900

    update option & fixed typo

--- uninstall.sh
+++ uninstall.sh
@@ -19,98 +19,61 @@ export PATH
 
 cur_dir=`pwd`
 
-upcase_to_lowcase(){
-    words=$1
-    echo $words | tr '[A-Z]' '[a-z]'
-}
-
-#Check system
-check_sys(){
-    local checkType=$1
-    local value=$2
-
-    local release=''
-    local systemPackage=''
-
-    if [[ -f /etc/redhat-release ]];then
-        release="centos"
-        systemPackage="yum"
-    elif cat /etc/issue | grep -q -E -i "debian";then
-        release="debian"
-        systemPackage="apt"
-    elif cat /etc/issue | grep -q -E -i "ubuntu";then
-        release="ubuntu"
-        systemPackage="apt"
-    elif cat /etc/issue | grep -q -E -i "centos|red hat|redhat";then
-        release="centos"
-        systemPackage="yum"
-    elif cat /proc/version | grep -q -E -i "debian";then
-        release="debian"
-        systemPackage="apt"
-    elif cat /proc/version | grep -q -E -i "ubuntu";then
-        release="ubuntu"
-        systemPackage="apt"
-    elif cat /proc/version | grep -q -E -i "centos|red hat|redhat";then
-        release="centos"
-        systemPackage="yum"
-    fi
-
-    if [[ ${checkType} == "sysRelease" ]]; then
-        if [ "$value" == "$release" ];then
-            return 0
-        else
-            return 1
-        fi
-    elif [[ ${checkType} == "packageManager" ]]; then
-        if [ "$value" == "$systemPackage" ];then
-            return 0
-        else
-            return 1
-        fi
+include(){
+    local include=$1
+    if [[ -s ${cur_dir}/include/${include}.sh ]];then
+        . ${cur_dir}/include/${include}.sh
+    else
+        echo "Error:${cur_dir}/include/${include}.sh not found, shell can not be executed."
+        exit 1
     fi
 }
 
+include config
+include public
 
-boot_stop(){
-    if check_sys packageManager apt;then
-        update-rc.d -f $1 remove
-    elif check_sys packageManager yum;then
-        chkconfig --del $1
-    fi
-}
+uninstall_lamp(){
 
-uninstall(){
+    load_config
 
     echo "uninstalling Apache"
-    [ -f /etc/init.d/httpd ] && /etc/init.d/httpd stop && boot_stop httpd
+    [ -f /etc/init.d/httpd ] && /etc/init.d/httpd stop > /dev/null 2>&1 && boot_stop httpd
     rm -f /etc/init.d/httpd
-    rm -rf /usr/local/apache /usr/sbin/httpd /var/log/httpd /etc/logrotate.d/httpd /var/spool/mail/apache
-    echo "Sucess"
+    rm -rf ${apache_location} /usr/sbin/httpd /var/log/httpd /etc/logrotate.d/httpd /var/spool/mail/apache
+    echo "Success"
 
     echo "uninstalling MySQL/MariaDB/Percona"
-    [ -f /etc/init.d/mysqld ] && /etc/init.d/mysqld stop && boot_stop mysqld
+    [ -f /etc/init.d/mysqld ] && /etc/init.d/mysqld stop > /dev/null 2>&1 && boot_stop mysqld
     rm -f /etc/init.d/mysqld
-    rm -rf /usr/local/mysql /usr/local/mariadb /usr/local/percona /usr/bin/mysqldump /usr/bin/mysql /etc/my.cnf /etc/ld.so.conf.d/mysql.conf
-    echo "Sucess"
+    rm -rf ${mysql_location} ${mariadb_location} ${percona_location} /usr/bin/mysqldump /usr/bin/mysql /etc/my.cnf /etc/ld.so.conf.d/mysql.conf
+    echo "Success"
 
     echo "uninstalling PHP"
-    rm -rf /usr/local/php /usr/bin/php /usr/bin/php-config /usr/bin/phpize /etc/php.ini
-    echo "Sucess"
+    rm -rf ${php_location} /usr/bin/php /usr/bin/php-config /usr/bin/phpize /etc/php.ini
+    echo "Success"
     echo
-    echo "uninstalling Others software"
-    [ -f /etc/init.d/memcached ] && /etc/init.d/memcached stop && boot_stop memcached
+    echo "uninstalling others software"
+    [ -f /etc/init.d/memcached ] && /etc/init.d/memcached stop > /dev/null 2>&1 && boot_stop memcached
     rm -f /etc/init.d/memcached
-    rm -fr /usr/local/memcached /usr/bin/memcached
-    [ -f /etc/init.d/redis-server ] && /etc/init.d/redis-server stop && boot_stop redis-server
+    rm -fr ${depends_prefix}/memcached /usr/bin/memcached
+    [ -f /etc/init.d/redis-server ] && /etc/init.d/redis-server stop > /dev/null 2>&1 && boot_stop redis-server
     rm -f /etc/init.d/redis-server
-    rm -rf /usr/local/redis
-    rm -rf /usr/local/libiconv /usr/lib64/libiconv.so.0 /usr/lib/libiconv.so.0
-    rm -rf /usr/local/imap-2007f
-    rm -rf /usr/local/pcre
+    rm -rf ${depends_prefix}/redis
+    rm -rf ${depends_prefix}/libiconv /usr/lib64/libiconv.so.0 /usr/lib/libiconv.so.0
+    rm -rf ${depends_prefix}/imap-2007f
+    rm -rf ${depends_prefix}/pcre
+    rm -rf ${openssl_location}
+    rm -rf /usr/lib/libnghttp2.*
+    rm -rf /usr/local/lib/libedit.*
+    rm -rf /usr/local/lib/libmcrypt.*
+    rm -rf /usr/local/lib/libmhash.*
+    rm -rf /usr/local/bin/re2c
+    rm -rf /usr/local/bin/mcrypt
+    rm -rf /usr/local/bin/mdecrypt
     rm -rf /etc/ld.so.conf.d/locallib.conf
-    rm -rf /data/www/default/phpmyadmin
-    rm -rf /data/www/default/xcache /tmp/{pcov,phpcore}
-    echo "Sucess"
+    rm -rf ${web_root_dir}/phpmyadmin
+    rm -rf ${web_root_dir}/xcache /tmp/{pcov,phpcore}
+    echo "Success"
     echo
     echo "Successfully uninstall LAMP!"
 }
@@ -121,8 +84,8 @@ do
     [ -z ${uninstall} ] && uninstall="n"
     uninstall="`upcase_to_lowcase ${uninstall}`"
     case ${uninstall} in
-        y) uninstall ; break;;
+        y) uninstall_lamp ; break;;
         n) echo "Uninstall cancelled, nothing to do" ; break;;
-        *) echo "Input error!";;
+        *) echo "Input error. Please only input y/n";;
     esac
 done
