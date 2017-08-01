ab6210d79a2011e37b6168f095d9ac4b6b64e425
Teddysun <i@teddysun.com>
2017-08-02 08:14:09 +0900

    update Percona Server version
    Signed-off-by: Teddysun <i@teddysun.com>

--- include/config.sh
+++ include/config.sh
@@ -59,7 +59,7 @@ mariadb10_2_filename="mariadb-10.2.7"
 #percona5.5
 percona5_5_filename="Percona-Server-5.5.55-38.8"
 #percona5.6
-percona5_6_filename="Percona-Server-5.6.36-82.0"
+percona5_6_filename="Percona-Server-5.6.36-82.1"
 #percona5.7
 percona5_7_filename="Percona-Server-5.7.18-16"
 #php5.3
