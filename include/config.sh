ba4adbd241d01c2865fdb92848111f0c8e092859
Teddysun <i@teddysun.com>
2017-08-26 11:06:15 +0900

    update software version
    Signed-off-by: Teddysun <i@teddysun.com>

--- include/config.sh
+++ include/config.sh
@@ -59,7 +59,7 @@ mariadb10_2_filename="mariadb-10.2.8"
 #percona5.5
 percona5_5_filename="Percona-Server-5.5.57-38.9"
 #percona5.6
-percona5_6_filename="Percona-Server-5.6.36-82.1"
+percona5_6_filename="Percona-Server-5.6.37-82.2"
 #percona5.7
 percona5_7_filename="Percona-Server-5.7.18-16"
 #php5.3
@@ -76,7 +76,7 @@ php7_0_filename="php-7.0.22"
 php7_1_filename="php-7.1.8"
 #phpMyAdmin
 phpmyadmin_filename="phpMyAdmin-4.4.15.10-all-languages"
-phpmyadmin_filename2="phpMyAdmin-4.7.3-all-languages"
+phpmyadmin_filename2="phpMyAdmin-4.7.4-all-languages"
 #opcache
 opcache_filename="zendopcache-7.0.5"
 
