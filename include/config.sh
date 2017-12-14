cc15a4c83a0a645fcfc94dfdbf2a3b52b19ff8a5
Teddysun <i@teddysun.com>
2017-12-15 08:45:58 +0900

    Update software version
    Signed-off-by: Teddysun <i@teddysun.com>

--- include/config.sh
+++ include/config.sh
@@ -59,7 +59,7 @@ percona5_5_filename="Percona-Server-5.5.58-38.10"
 #percona5.6
 percona5_6_filename="Percona-Server-5.6.38-83.0"
 #percona5.7
-percona5_7_filename="Percona-Server-5.7.19-17"
+percona5_7_filename="Percona-Server-5.7.20-18"
 #php5.6
 php5_6_filename="php-5.6.32"
 #php7.0
