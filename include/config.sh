ab1dcf7aab0b808baa50836071ff035c6ef6d6ca
Teddysun <i@teddysun.com>
2018-06-23 10:33:15 +0900

    Update software version
    Signed-off-by: Teddysun <i@teddysun.com>

--- include/config.sh
+++ include/config.sh
@@ -73,7 +73,7 @@ php7_1_filename="php-7.1.18"
 #php7.2
 php7_2_filename="php-7.2.7"
 #phpMyAdmin
-phpmyadmin_filename="phpMyAdmin-4.8.1-all-languages"
+phpmyadmin_filename="phpMyAdmin-4.8.2-all-languages"
 
 #apr
 apr_filename="apr-1.6.3"
