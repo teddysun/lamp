57a05576c0c0b8c749923cd2e787e72fe486a002
Teddysun <i@teddysun.com>
2018-08-15 21:09:08 +0900

    Update software version
    Signed-off-by: Teddysun <i@teddysun.com>

--- include/config.sh
+++ include/config.sh
@@ -55,7 +55,7 @@ mariadb10_0_filename="mariadb-10.0.36"
 #mariadb10.1
 mariadb10_1_filename="mariadb-10.1.35"
 #mariadb10.2
-mariadb10_2_filename="mariadb-10.2.16"
+mariadb10_2_filename="mariadb-10.2.17"
 #mariadb10.3
 mariadb10_3_filename="mariadb-10.3.8"
 #percona5.5
