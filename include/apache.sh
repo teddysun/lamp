bcc8ac5bb0fe9e767cc4e1de3da2086f56fca6a5
Teddysun <i@teddysun.com>
2018-12-02 20:33:46 +0900

    Update comments
    Signed-off-by: Teddysun <i@teddysun.com>

--- include/apache.sh
+++ include/apache.sh
@@ -33,7 +33,7 @@ apache_preinstall_settings(){
 install_apache(){
 
     log "Info" "Starting to install dependencies packages for Apache..."
-    local apt_list=(openssl libssl-dev libxml2-dev lynx lua-expat-dev libjansson-dev)
+    local apt_list=(zlib1g-dev openssl libssl-dev libxml2-dev lynx lua-expat-dev libjansson-dev)
     local yum_list=(zlib-devel openssl-devel libxml2-devel lynx expat-devel lua-devel lua jansson-devel)
     if check_sys packageManager apt; then
         for depend in ${apt_list[@]}; do
