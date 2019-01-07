83ac668d4b2f819e3bf2741d44b822bc324aa3ff
Teddysun <i@teddysun.com>
2019-01-08 08:52:58 +0900

    Update MariaDB version
    Signed-off-by: Teddysun <i@teddysun.com>

--- include/config.sh
+++ include/config.sh
@@ -57,7 +57,7 @@ mariadb10_1_filename="mariadb-10.1.37"
 #mariadb10.2
 mariadb10_2_filename="mariadb-10.2.21"
 #mariadb10.3
-mariadb10_3_filename="mariadb-10.3.11"
+mariadb10_3_filename="mariadb-10.3.12"
 #percona5.5
 percona5_5_filename="Percona-Server-5.5.62-38.14"
 #percona5.6
