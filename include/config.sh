6ad9ae412d550ed89f198906fdf8fa5152aa8266
Teddysun <i@teddysun.com>
2019-02-07 08:58:32 +0900

    Update MariaDB version
    Signed-off-by: Teddysun <i@teddysun.com>

--- include/config.sh
+++ include/config.sh
@@ -53,7 +53,7 @@ mariadb5_5_filename="mariadb-5.5.63"
 #mariadb10.0
 mariadb10_0_filename="mariadb-10.0.38"
 #mariadb10.1
-mariadb10_1_filename="mariadb-10.1.37"
+mariadb10_1_filename="mariadb-10.1.38"
 #mariadb10.2
 mariadb10_2_filename="mariadb-10.2.21"
 #mariadb10.3
