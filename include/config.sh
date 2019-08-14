e339c9777bc816d8368e9bfe606a5364bd68e34b
Teddysun <i@teddysun.com>
2019-08-14 09:16:37 +0900

    Update nghttp2 version
    Signed-off-by: Teddysun <i@teddysun.com>

--- include/config.sh
+++ include/config.sh
@@ -35,8 +35,8 @@ parallel_compile=1
 
 ##Software version
 #nghttp2
-nghttp2_filename="nghttp2-1.39.1"
-nghttp2_filename_url="https://github.com/nghttp2/nghttp2/releases/download/v1.39.1/nghttp2-1.39.1.tar.gz"
+nghttp2_filename="nghttp2-1.39.2"
+nghttp2_filename_url="https://github.com/nghttp2/nghttp2/releases/download/v1.39.2/nghttp2-1.39.2.tar.gz"
 #openssl
 openssl_filename="openssl-1.1.1c"
 openssl_filename_url="https://www.openssl.org/source/openssl-1.1.1c.tar.gz"
