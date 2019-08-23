466ca7b1a99097113243a91159d6c9e2e108018e
Teddysun <i@teddysun.com>
2019-08-23 18:26:17 +0900

    Update Percona version

--- include/config.sh
+++ include/config.sh
@@ -68,7 +68,7 @@ percona5_5_filename="Percona-Server-5.5.62-38.14"
 #percona5.6
 percona5_6_filename="Percona-Server-5.6.45-86.1"
 #percona5.7
-percona5_7_filename="Percona-Server-5.7.26-29"
+percona5_7_filename="Percona-Server-5.7.27-30"
 #percona8.0
 percona8_0_filename="Percona-Server-8.0.16-7"
 #php5.6
