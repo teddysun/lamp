1f71653e387fcb5ee0f7ac5e51d71e42b4b760f8
Teddysun <i@teddysun.com>
2019-09-12 23:40:52 +0900

    Update MariaDB version

--- include/config.sh
+++ include/config.sh
@@ -58,11 +58,11 @@ mariadb10_0_filename="mariadb-10.0.38"
 #mariadb10.1
 mariadb10_1_filename="mariadb-10.1.41"
 #mariadb10.2
-mariadb10_2_filename="mariadb-10.2.26"
+mariadb10_2_filename="mariadb-10.2.27"
 #mariadb10.3
-mariadb10_3_filename="mariadb-10.3.17"
+mariadb10_3_filename="mariadb-10.3.18"
 #mariadb10.4
-mariadb10_4_filename="mariadb-10.4.7"
+mariadb10_4_filename="mariadb-10.4.8"
 #percona5.5
 percona5_5_filename="Percona-Server-5.5.62-38.14"
 #percona5.6
