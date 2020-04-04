b5a7d1cf7b620e0db46900514f98f044735aee02
Teddysun <i@teddysun.com>
2020-04-04 17:18:08 +0900

    Update openssl version

--- include/config.sh
+++ include/config.sh
@@ -38,8 +38,8 @@ parallel_compile=1
 nghttp2_filename="nghttp2-1.40.0"
 nghttp2_filename_url="https://github.com/nghttp2/nghttp2/releases/download/v1.40.0/nghttp2-1.40.0.tar.gz"
 #openssl
-openssl_filename="openssl-1.1.1d"
-openssl_filename_url="https://www.openssl.org/source/openssl-1.1.1d.tar.gz"
+openssl_filename="openssl-1.1.1f"
+openssl_filename_url="https://www.openssl.org/source/openssl-1.1.1f.tar.gz"
 #apache2.4
 apache2_4_filename="httpd-2.4.43"
 apache2_4_filename_url="http://ftp.jaist.ac.jp/pub/apache//httpd/httpd-2.4.43.tar.gz"
