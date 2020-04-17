420688467206dd6877c1dae7296cad4a1869b723
Teddysun <i@teddysun.com>
2020-04-17 17:11:56 +0900

    Update PHP version

--- include/config.sh
+++ include/config.sh
@@ -81,11 +81,11 @@ php7_0_filename_url="https://www.php.net/distributions/php-7.0.33.tar.gz"
 php7_1_filename="php-7.1.33"
 php7_1_filename_url="https://www.php.net/distributions/php-7.1.33.tar.gz"
 #php7.2
-php7_2_filename="php-7.2.29"
-php7_2_filename_url="https://www.php.net/distributions/php-7.2.29.tar.gz"
+php7_2_filename="php-7.2.30"
+php7_2_filename_url="https://www.php.net/distributions/php-7.2.30.tar.gz"
 #php7.3
-php7_3_filename="php-7.3.16"
-php7_3_filename_url="https://www.php.net/distributions/php-7.3.16.tar.gz"
+php7_3_filename="php-7.3.17"
+php7_3_filename_url="https://www.php.net/distributions/php-7.3.17.tar.gz"
 #phpMyAdmin
 phpmyadmin_filename="phpMyAdmin-4.9.5-all-languages"
 phpmyadmin_filename_url="https://files.phpmyadmin.net/phpMyAdmin/4.9.5/phpMyAdmin-4.9.5-all-languages.tar.gz"
