360252d44478c71acfb947f71959879a99f6db03
Teddysun <i@teddysun.com>
2020-10-27 16:53:11 +0900

    Update software version

--- include/config.sh
+++ include/config.sh
@@ -199,13 +199,13 @@ php_memcached_filename_url="https://pecl.php.net/get/memcached-2.2.0.tgz"
 php_memcached_filename2="memcached-3.1.5"
 php_memcached_filename2_url="https://pecl.php.net/get/memcached-3.1.5.tgz"
 #redis
-redis_filename="redis-5.0.9"
-redis_filename_url="http://download.redis.io/releases/redis-5.0.9.tar.gz"
+redis_filename="redis-5.0.10"
+redis_filename_url="http://download.redis.io/releases/redis-5.0.10.tar.gz"
 #php extension redis
 php_redis_filename="redis-4.3.0"
 php_redis_filename_url="https://pecl.php.net/get/redis-4.3.0.tgz"
-php_redis_filename2="redis-5.3.1"
-php_redis_filename2_url="https://pecl.php.net/get/redis-5.3.1.tgz"
+php_redis_filename2="redis-5.3.2"
+php_redis_filename2_url="https://pecl.php.net/get/redis-5.3.2.tgz"
 #php extension mongodb
 php_mongo_filename="mongodb-1.8.0"
 php_mongo_filename_url="https://pecl.php.net/get/mongodb-1.8.0.tgz"
@@ -225,8 +225,8 @@ psr_filename_url="https://pecl.php.net/get/psr-1.0.0.tgz"
 phalcon_filename="phalcon-4.0.6"
 phalcon_filename_url="https://pecl.php.net/get/phalcon-4.0.6.tgz"
 #php extension apcu
-apcu_filename="apcu-5.1.18"
-apcu_filename_url="https://pecl.php.net/get/apcu-5.1.18.tgz"
+apcu_filename="apcu-5.1.19"
+apcu_filename_url="https://pecl.php.net/get/apcu-5.1.19.tgz"
 #php extension grpc
 grpc_filename="grpc-1.33.1"
 grpc_filename_url="https://pecl.php.net/get/grpc-1.33.1.tgz"
