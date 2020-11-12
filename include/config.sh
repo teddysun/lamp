4e19a96b7e8055ec62a8b08ecc64a1d3cdec2abd
Teddysun <i@teddysun.com>
2020-11-12 10:26:41 +0900

    Update MariaDB version

--- include/config.sh
+++ include/config.sh
@@ -51,13 +51,13 @@ mysql8_0_filename="mysql-8.0.22"
 #mariadb10.1
 mariadb10_1_filename="mariadb-10.1.48"
 #mariadb10.2
-mariadb10_2_filename="mariadb-10.2.35"
+mariadb10_2_filename="mariadb-10.2.36"
 #mariadb10.3
-mariadb10_3_filename="mariadb-10.3.26"
+mariadb10_3_filename="mariadb-10.3.27"
 #mariadb10.4
-mariadb10_4_filename="mariadb-10.4.16"
+mariadb10_4_filename="mariadb-10.4.17"
 #mariadb10.5
-mariadb10_5_filename="mariadb-10.5.7"
+mariadb10_5_filename="mariadb-10.5.8"
 #php5.6
 php5_6_filename="php-5.6.40"
 php5_6_filename_url="https://www.php.net/distributions/php-5.6.40.tar.gz"
