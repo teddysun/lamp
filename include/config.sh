470dea59dd19fe68bb599060a77d83336fc610ab
Teddysun <i@teddysun.com>
2021-04-21 10:24:37 +0900

    Update comments update MySQL to version 5.7.34, 8.0.24

--- include/config.sh
+++ include/config.sh
@@ -45,9 +45,9 @@ apache2_4_filename_url="http://ftp.jaist.ac.jp/pub/apache//httpd/httpd-2.4.46.ta
 #mysql5.6
 mysql5_6_filename="mysql-5.6.51"
 #mysql5.7
-mysql5_7_filename="mysql-5.7.33"
+mysql5_7_filename="mysql-5.7.34"
 #mysql8.0
-mysql8_0_filename="mysql-8.0.23"
+mysql8_0_filename="mysql-8.0.24"
 #mariadb10.1
 mariadb10_1_filename="mariadb-10.1.48"
 #mariadb10.2
