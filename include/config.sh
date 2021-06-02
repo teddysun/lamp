128a5357b1767156af75664fe622430c3a70fa8c
Teddysun <i@teddysun.com>
2021-06-03 07:27:08 +0900

    Update comments update httpd to version 2.4.48
    Signed-off-by: Teddysun <i@teddysun.com>

--- include/config.sh
+++ include/config.sh
@@ -40,8 +40,8 @@ nghttp2_filename_url="https://github.com/nghttp2/nghttp2/releases/download/v1.43
 openssl_filename="openssl-1.1.1k"
 openssl_filename_url="https://www.openssl.org/source/openssl-1.1.1k.tar.gz"
 #apache2.4
-apache2_4_filename="httpd-2.4.46"
-apache2_4_filename_url="http://ftp.jaist.ac.jp/pub/apache//httpd/httpd-2.4.46.tar.gz"
+apache2_4_filename="httpd-2.4.48"
+apache2_4_filename_url="http://ftp.jaist.ac.jp/pub/apache//httpd/httpd-2.4.48.tar.gz"
 #mysql5.6
 mysql5_6_filename="mysql-5.6.51"
 #mysql5.7
