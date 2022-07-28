8137ec464789fec897dc74690b02cf9cbf6994a4
Teddysun <i@teddysun.com>
2022-07-28 16:42:08 +0900

    Update comments Update MySQL to version 5.7.39, 8.0.30
    Signed-off-by: Teddysun <i@teddysun.com>

--- include/config.sh
+++ include/config.sh
@@ -43,9 +43,9 @@ openssl_filename_url="https://www.openssl.org/source/openssl-1.1.1q.tar.gz"
 apache2_4_filename="httpd-2.4.54"
 apache2_4_filename_url="https://dlcdn.apache.org//httpd/httpd-2.4.54.tar.gz"
 #mysql5.7
-mysql5_7_filename="mysql-5.7.38"
+mysql5_7_filename="mysql-5.7.39"
 #mysql8.0
-mysql8_0_filename="mysql-8.0.29"
+mysql8_0_filename="mysql-8.0.30"
 #mariadb10.2
 mariadb10_2_filename="mariadb-10.2.44"
 #mariadb10.3
