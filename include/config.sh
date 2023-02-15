3cf030e5aa26e82575c1aa19f45746779f8d46fc
Teddysun <i@teddysun.com>
2023-02-15 11:25:34 +0900

    Update comments Update PHP to version 8.0.28, 8.1.16
    Signed-off-by: Teddysun <i@teddysun.com>

--- include/config.sh
+++ include/config.sh
@@ -60,11 +60,11 @@ mariadb10_7_filename="mariadb-10.7.8"
 php7_4_filename="php-7.4.33"
 php7_4_filename_url="https://www.php.net/distributions/php-7.4.33.tar.gz"
 #php8.0
-php8_0_filename="php-8.0.27"
-php8_0_filename_url="https://www.php.net/distributions/php-8.0.27.tar.gz"
+php8_0_filename="php-8.0.28"
+php8_0_filename_url="https://www.php.net/distributions/php-8.0.28.tar.gz"
 #php8.1
-php8_1_filename="php-8.1.15"
-php8_1_filename_url="https://www.php.net/distributions/php-8.1.15.tar.gz"
+php8_1_filename="php-8.1.16"
+php8_1_filename_url="https://www.php.net/distributions/php-8.1.16.tar.gz"
 #phpMyAdmin
 phpmyadmin_filename="phpMyAdmin-5.2.1-all-languages"
 phpmyadmin_filename_url="https://files.phpmyadmin.net/phpMyAdmin/5.2.1/phpMyAdmin-5.2.1-all-languages.tar.gz"
