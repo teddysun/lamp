8fd52d00de16d8ad410733bc8b130b44fa4857a0
Teddysun <i@teddysun.com>
2023-12-17 12:57:01 +0900

    Update comments Update PHP to version 8.1.26, 8.2.13
    Signed-off-by: Teddysun <i@teddysun.com>

--- include/config.sh
+++ include/config.sh
@@ -58,11 +58,11 @@ load_config() {
     php8_0_filename="php-8.0.30"
     php8_0_filename_url="https://www.php.net/distributions/php-8.0.30.tar.gz"
     #php8.1
-    php8_1_filename="php-8.1.25"
-    php8_1_filename_url="https://www.php.net/distributions/php-8.1.25.tar.gz"
+    php8_1_filename="php-8.1.26"
+    php8_1_filename_url="https://www.php.net/distributions/php-8.1.26.tar.gz"
     #php8.2
-    php8_2_filename="php-8.2.12"
-    php8_2_filename_url="https://www.php.net/distributions/php-8.2.12.tar.gz"
+    php8_2_filename="php-8.2.13"
+    php8_2_filename_url="https://www.php.net/distributions/php-8.2.13.tar.gz"
     #phpMyAdmin
     phpmyadmin_filename="phpMyAdmin-5.2.1-all-languages"
     phpmyadmin_filename_url="https://files.phpmyadmin.net/phpMyAdmin/5.2.1/phpMyAdmin-5.2.1-all-languages.tar.gz"
