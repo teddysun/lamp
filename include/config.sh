a7516c85dbdd8c1b5a62f369da15cfb7c687e106
Teddysun <i@teddysun.com>
2024-12-20 16:09:46 +0900

    Update comments

--- include/config.sh
+++ include/config.sh
@@ -59,8 +59,8 @@ load_config() {
     php8_1_filename="php-8.1.31"
     php8_1_filename_url="https://www.php.net/distributions/php-8.1.31.tar.gz"
     #php8.2
-    php8_2_filename="php-8.2.26"
-    php8_2_filename_url="https://www.php.net/distributions/php-8.2.26.tar.gz"
+    php8_2_filename="php-8.2.27"
+    php8_2_filename_url="https://www.php.net/distributions/php-8.2.27.tar.gz"
     #phpMyAdmin
     phpmyadmin_filename="phpMyAdmin-5.2.1-all-languages"
     phpmyadmin_filename_url="https://files.phpmyadmin.net/phpMyAdmin/5.2.1/phpMyAdmin-5.2.1-all-languages.tar.gz"
