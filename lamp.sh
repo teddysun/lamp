c4683babb569dbdbfb5cd0a646150923819bffdc
Teddysun <i@teddysun.com>
2025-12-29 13:20:33 +0900

    Update comments Added support PHP major version 8.5
    Signed-off-by: Teddysun <i@teddysun.com>

--- lamp.sh
+++ lamp.sh
@@ -306,6 +306,7 @@ while true; do
     _info "$(_green 4). PHP 8.2"
     _info "$(_green 5). PHP 8.3"
     _info "$(_green 6). PHP 8.4"
+    _info "$(_green 7). PHP 8.5"
     read -r -p "[$(date)] Please input a number: (Default 6) " php_version
     [ -z "${php_version}" ] && php_version=6
     case "${php_version}" in
@@ -333,8 +334,12 @@ while true; do
         php_ver="8.4"
         break
         ;;
+    7)
+        php_ver="8.5"
+        break
+        ;;
     *)
-        _info "Input error. Please input a number between 1 and 6"
+        _info "Input error. Please input a number between 1 and 7"
         ;;
     esac
 done
